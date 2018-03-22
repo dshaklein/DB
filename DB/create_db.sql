8fca41d72f3980aa98b19fac30f03ed4c410bff8
Daniel.ef <dlefimov@mail.ru>
2018-03-23 00:34:11 +0300

    Changes

--- DB/create_db.sql
+++ DB/create_db.sql
@@ -12,8 +12,7 @@ CREATE TABLE "User" (
 
 CREATE TABLE "Group" (
 	"id" serial NOT NULL,
-	"write" bool NOT NULL,
-	"read" bool NOT NULL,
+	"edit" bool NOT NULL,
 	"comment" bool NOT NULL,
 	"name" character varying(20) NOT NULL,
 	CONSTRAINT Group_pk PRIMARY KEY ("id")
@@ -65,7 +64,7 @@ CREATE TABLE "FilmEmotion" (
 
 CREATE TABLE "Emotion" (
 	"id" serial NOT NULL,
-	"combination" integer NOT NULL,
+	"combination" integer[3] NOT NULL DEFAULT '{0,0,0}',
 	"title" character varying(60) NOT NULL,
 	CONSTRAINT Emotion_pk PRIMARY KEY ("id")
 ) WITH (
@@ -115,4 +114,3 @@ ALTER TABLE "UserWatch" ADD CONSTRAINT "UserWatch_fk1" FOREIGN KEY ("film_id") R
 ALTER TABLE "UserRating" ADD CONSTRAINT "UserRating_fk0" FOREIGN KEY ("user_id") REFERENCES "User"("id");
 ALTER TABLE "UserRating" ADD CONSTRAINT "UserRating_fk1" FOREIGN KEY ("film_id") REFERENCES "Film"("id");
 ALTER TABLE "UserRating" ADD CONSTRAINT "UserRating_fk2" FOREIGN KEY ("emotion_id") REFERENCES "Emotion"("id");
-
